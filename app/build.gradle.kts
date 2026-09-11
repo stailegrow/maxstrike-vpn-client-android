38151d28a75f5c57f32b617370a7fa92013676c9
StaileGroW <arigabiproksa@gmail.com>
2026-09-11 14:45:03 +0000

    Обновляю androidx.activity и core-ktx до актуальных стабильных версий

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.core:core-ktx:1.19.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
